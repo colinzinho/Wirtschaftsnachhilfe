0a0e46f7fcd1b526e5d71dcb7ca70014fbc3e4d3
colinzinho <colin.spichiger@gmx.ch>
2025-01-02 09:20:39 +0100

    1.Resolved scrolling problems; 2.Removed all unnecessary files;

--- js/pageLogic.js
+++ js/pageLogic.js
@@ -1,50 +1,113 @@
+let windowpos;
 let header;
-let pageContainer;
+let mainContentContainer;
 let toTopBtn;
-let thresholdToTopBtn = -1;
-const imaginarySpace = 20; /* stands for the imagniary space in px above the header */ 
+let thresholdToTopBtn;
+let headerIsFixed;
+const imaginarySpace = 20; /* Slows down the header animation by preventing the animation to start as user starts to scroll */ 
 
 $(document).ready(function() {
     /* Set variable values after initialization of site */
+    windowpos = $(window).scrollTop(); /* Gets vertical scrollbar position of window element(top left corner of window) */
     header = $("#page-header");
-    pageContainer = $("#page-container");
+    mainContentContainer = $("#main-content");
     toTopBtn = $("#to-top-button");
 });
 
+function onClick() {
+    /* Reload as Logo in top left corner is clicked */
+    $("#logo-link").click(function (event) {
+        window.location.reload(0);
+    });
+
+    /* Sets behavior as links in navbar are being clicked */
+    $("#navigation a").click(function (event) {
+        let sectionName = $(this).data("nav-section"); 
+        scrollingTo(sectionName);
+    });
+
+    /* Arrow up button which hides in the bottom right corner of the website */
+    $("#to-top-button").click(function (event) {
+        window.history.pushState('', '', '/index.html');
+        $(window).scrollTop(0)
+    });
+};
+
+/* Sets behavior as one of the various contact buttons is being clicked  */
+function onContactBtnClicked(event) {
+    scrollingTo('contact');
+}
+
 $(window).scroll(function() {
-    let windowpos = $(window).scrollTop(); /* gets vertical scrollbar position of window element(top left corner of window) */
-    let headerIsFixed = header.hasClass("fixed-header");
-    let btnIsHidden = toTopBtn.hasClass("hidden");
+    windowpos = $(window).scrollTop(); /* Gets vertical scrollbar position of window element(top left corner of window) */
+    headerIsFixed = header.hasClass("fixed-header");
 
     if(windowpos > imaginarySpace && !headerIsFixed) {
-        header.addClass("fixed-header"); /* Changes/ decreases height of header */
-        thresholdToTopBtn = $('div[id="offers"]').offset().top - getHeaderHeight(); /* Estimates 'space' between bottom left corner of navbar and top left corner of offers section  */
-        if($(window).width() < 625) {
-            thresholdToTopBtn -= 20; /* If not set 'toTopBtn' shows to late and hides to early... */
-        }
-        pageContainer.addClass("animation"); /* enables smooth animation (ease-in-ease-out) */
-        pageContainer.css('margin-top', -41 + 'px'); /* whole website content hops up by 41px - is being used for smooth animation/ transition */
-    }else if(windowpos <= 0 && headerIsFixed) { /* restore default size of header (height) */
-        header.removeClass("fixed-header");
-        pageContainer.css('margin-top', -1 + 'px'); /* whole website content hops down by 40px - is being used for smooth animation/ transition */
+        header.addClass("fixed-header"); /* Changes decrease height of header */
+        scrollAnimationHeader(-40);
+        thresholdToTopBtn = Math.round($('div[id="offers"]').offset().top) - getHeaderHeight(); /* Estimates 'space' between bottom left corner of navbar and top left corner of offers section  */
+    }
+    
+    if(windowpos <= 0 && headerIsFixed) {
+        header.removeClass("fixed-header"); /* Restore default size of header (height) */
+        scrollAnimationHeader(40);
     }
 
-    /* thresholdToTopBtn only turns positive value as website has being scrolled by more than 20px */
-    if(thresholdToTopBtn !== -1) {
-        toTopBtnHandling(windowpos, btnIsHidden);
-    }
+    toTopBtnHandling(windowpos, toTopBtn.hasClass("hidden"));
 });
+
+/* Helper function: animates main content by setting it's 'margin-top'. The resulting animation is ether hop up or down! */
+function scrollAnimationHeader(amount) {
+    if(!mainContentContainer.hasClass("animation")) {
+        mainContentContainer.addClass("animation"); /* Can't be set by default in css! 'margin-top' is being set while web page built and otherwise it would apply smooth transition effect while building web page... */
+    }
+    const decreasedValue = parseInt(mainContentContainer.css('margin-top')) + amount; /* Without parsing jQuery .css function returns number + px at the end */
+    mainContentContainer.css('margin-top', decreasedValue + 'px'); /* Main website content hops up by 40px - is being used for smooth animation/ transition */
+    /* If amount is bigger than 0/positive then we arrived at the top of the page and the value for windowpos remains 0! */
+    if(amount < 0) {
+        windowpos += 40; /* $(window).scrollTop() value is not affected by negative margin-top (main website content hops up)! */
+    }
+}
+
+/* Helper function: enables scrolling on whole webpage */
+function scrollingTo(input) {
+    let sectionName = input.toLowerCase();
+    let headerHeight = getHeaderHeight();
+    if(!header.hasClass("fixed-header")){
+        if($(window).width() < 625) {
+            headerHeight += 20; /* The difference in the sum of padding top plus bottom of fixed and collapsed header on small devices is only 20px... as soon as main content hops up by 40px it hops up too far by 20px! */
+        }
+        header.addClass("fixed-header"); /* Changes decrease height of header */
+        scrollAnimationHeader(-40);
+    }
+
+    thresholdToTopBtn = Math.round($('div[id="' + sectionName + '"]').offset().top) - headerHeight; /* Estimates 'space' between bottom left corner of navbar and top left corner of offers section  */
+    $("html").animate(
+        {
+        scrollTop:
+            $('div[data-section="' + sectionName + '"]').offset().top - headerHeight + 1
+        },
+        250
+    );
+    /* Hide collapsed site menu after being clicked and routing completed */
+    if($("#menubutton").hasClass('show-menu')) {
+        $("#menubutton").removeClass('show-menu');
+    }
+};
 
 /* Controls Visibility of 'backToTop' button */
 function toTopBtnHandling(windowpos, btnIsHidden) {
-if(windowpos >= thresholdToTopBtn && btnIsHidden) {
-    if(toTopBtn.hasClass("fadeOut")) {
-        toTopBtn.removeClass("fadeOut");
-    }
-    showElement(toTopBtn);
+    if(windowpos >= thresholdToTopBtn && btnIsHidden) {
+        if(toTopBtn.hasClass("fadeOut")) {
+            toTopBtn.removeClass("fadeOut");
+        }
+        showElement(toTopBtn);
     }
     if(windowpos < thresholdToTopBtn && !btnIsHidden) {
-    hideElementSmoothly(toTopBtn);
+        hideElementSmoothly(toTopBtn);
+    }
+    if(btnIsHidden == undefined) {
+        showElement(toTopBtn);
     }
 };
 
@@ -62,66 +125,7 @@ function hideElementSmoothly(element) {
     }, 2000);
 };
 
-function onClick() {
-    /* Reload as Logo in top left corner is clicked */
-    $("#logo-link").click(function (event) {
-        window.location.reload(0);
-    });
-
-    /* Sets behavior as links in navbar are being clicked */
-    $("#navigation a").click(function (event) {
-        let sectionName = $(this).data("nav-section"); 
-        let result = scrollingTo(sectionName);
-        if(!result) {
-        event.preventDefault(); // what does this do - exception handler??
-        }
-    });
-
-    
-    $(".contact-btn").click(function (event) {
-        
-    });
-
-    /* Arrow up button which hides in the bottom right corner of the website */
-    $("#to-top-button").click(function (event) {
-        window.history.pushState('', '', '/index.html');
-        $(window).scrollTop(0)
-    });
-};
-
-/* Sets behavior as one of the various contact buttons is being clicked  */
-function onContactBtnClicked(event) {
-    let result = scrollingTo('contact');
-    if(!result) {
-    event.preventDefault(); // what does this do - exception handler??
-    }
-}
-
-/* Helper function: enables scrolling on whole webpage */
-function scrollingTo(input) {
-    let sectionName = input.toLowerCase();
-    /* If not set to 19 on small devices (< 625px) scrolling mechanism scrolls to far */
-    let collapsingValueNavBar = (!header.hasClass('fixed-header') && $(window).width() < 625) ? 19 : -1;
-
-    if ($('div[data-section="' + sectionName + '"]').length) {
-    let headerHeight = getHeaderHeight() + collapsingValueNavBar;
-    $("html").animate(
-        {
-        scrollTop:
-            $('div[data-section="' + sectionName + '"]').offset().top - headerHeight
-        },
-        250
-    );
-    if($("#menubutton").hasClass('show-menu')) {
-        $("#menubutton").removeClass('show-menu');
-    }
-    return true;
-    } else {
-    return false;
-    }
-};
-
 /* Helper function: get current height of webpage header */
 function getHeaderHeight() {
-    return $('#page-header')[0].getBoundingClientRect().height;
+    return $('#page-header').outerHeight();
 };
